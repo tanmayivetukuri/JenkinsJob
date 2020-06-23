e4ad7a9cd2869cd2634b03963baf405a05e58306
Yadavbipin31 <34956125+Yadavbipin31@users.noreply.github.com>
2020-06-23 14:06:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("-------HelloWorld This is my first java Program------");
+		System.out.println("******HelloWorld This is my second java Program******");
 	}
 }
