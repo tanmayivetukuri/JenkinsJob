e3a6b93288c8a385997a59c42b93fe29ffce1ae9
MasterBY <34956125+Yadavbipin31@users.noreply.github.com>
2020-09-24 20:09:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("****** This is Github project Perfect for the First Time ******");
+		Sssystem.out.println("****** This is Github project Perfect for the second Time ******");
 	}
 }
