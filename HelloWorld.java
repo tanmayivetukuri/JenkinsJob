0d780fe6ec0a79f9403bb2f8f9a608e957f6366a
tanmayivetukuri <63117592+tanmayivetukuri@users.noreply.github.com>
2020-09-30 15:04:48 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("****** This is new number 6 ******");
+		System.out.println("****** This is task 1 ******");
 	}
 }
