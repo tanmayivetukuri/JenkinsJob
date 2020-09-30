6b8750c397322374d007be32407f14f5cb749ed8
tanmayivetukuri <63117592+tanmayivetukuri@users.noreply.github.com>
2020-09-30 15:51:42 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("****** This is to send email notification ******");
+		System.out.println("****** This is to send email notification updated ******");
 	}
 }
