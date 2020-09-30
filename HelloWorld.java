0db858906f5368e7f8fc75a11df61df194985c58
tanmayivetukuri <63117592+tanmayivetukuri@users.noreply.github.com>
2020-09-30 15:25:07 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("****** This is task 2 ******");
+		System.out.println("****** This is to send email notification ******");
 	}
 }
